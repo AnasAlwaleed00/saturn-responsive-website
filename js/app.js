12b5ac6e3cba554168040a34ab28986b67dcb2d3
Anas Alwaleed <anasalwaleed77@gmail.com>
2020-08-26 05:33:57 +0200

    -Added FAQ

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ const menuBtn = document.querySelector(".menu-icon span");
 const searchBtn = document.querySelector(".search-icon");
 const cancelBtn = document.querySelector(".cancel-icon");
 const items = document.querySelector(".nav-items");
+const que = document.querySelector(".que")
+const ques = document.querySelectorAll(".que")
+const queBtn = document.querySelector(".que button")
+const queBtns = document.querySelectorAll(".que button")
 
 menuBtn.onclick = () => {
     items.classList.add("active");
@@ -16,4 +20,20 @@ cancelBtn.onclick = () => {
     cancelBtn.style.color = "#ff3d00";
 }
 
+// queBtn.addEventListener("click", function(){
+//     que.classList.toggle("show")
+// })
 
+// queBtns.forEach(function(btn){
+//     btn.addEventListener("click", function(){
+//         que.classList.toggle("show")
+//     })
+// })
+console.log(queBtn.parentElement.parentElement.nextElementSibling)
+
+for (var i = 0; i < queBtns.length; i++) {
+    queBtns[i].onclick = function() {
+    //   this.classList.toggle("active");
+      this.parentElement.parentElement.nextElementSibling.classList.toggle("show");
+    };
+  }
